4c474479455bbf1843e9412f1b0ba74744852201
LucasCrespo75 <lucascrespo_rodrigues@live.com>
2023-12-01 17:42:13 -0300

    FEAT ADD: Responsividade: PAGINA INICIAL e Cadastro

---
+++ projeto-test/src/components/Register/index.jsx
@@ -0,0 +1,42 @@
+import React from "react";
+
+export const Register = () => {
+  return (
+    <div className="bg-[#0e2c40] flex flex-row justify-center w-full">
+      <div className="bg-[#0e2c40] overflow-hidden w-[390px] h-[855px] relative">
+        <div className="absolute w-[398px] h-[75px] top-[73px] left-[15px]">
+          <img className="absolute w-[222px] h-[75px] top-0 left-0" alt="Group" src="group-17.png" />
+          <div className="absolute w-[181px] h-[44px] top-[23px] left-[217px]">
+            <div className="absolute w-[179px] top-0 left-0 [font-family:'Inter-ExtraBold',Helvetica] font-extrabold text-[#018bd8] text-[24px] tracking-[0] leading-[normal]">
+              Listagem
+            </div>
+          </div>
+        </div>
+        <div className="absolute w-[313px] h-[395px] top-[171px] left-[33px]">
+          <img className="absolute w-[313px] h-[378px] top-0 left-0" alt="Group" src="group-15.png" />
+          <div className="absolute w-[70px] h-[30px] top-[312px] left-[199px]">
+            <div className="relative w-[68px] h-[30px] bg-[#0e2c40] rounded-[10px]">
+              <div className="absolute w-[46px] top-[6px] left-[11px] [font-family:'Inter-Regular',Helvetica] font-normal text-white text-[14px] tracking-[0] leading-[normal]">
+                Limpar
+              </div>
+            </div>
+          </div>
+          <img className="absolute w-[84px] h-[42px] top-[353px] left-[119px]" alt="Group" src="group-16.png" />
+          <div className="absolute w-[124px] h-[12px] top-[288px] left-[46px]">
+            <div className="absolute w-[10px] h-[10px] top-0 left-0 bg-[#5f5f5f]" />
+            <div className="absolute top-0 left-[14px] [font-family:'Inter-Regular',Helvetica] font-normal text-black text-[10px] tracking-[0] leading-[normal] whitespace-nowrap">
+              Termos de privacidade
+            </div>
+          </div>
+        </div>
+        <div className="absolute w-[128px] h-[67px] top-[630px] left-[132px]">
+          <div className="relative w-[126px] h-[67px] bg-[#183e58] rounded-[5px] shadow-[0px_4px_4px_#00000040]">
+            <div className="absolute w-[81px] top-[17px] left-[30px] [font-family:'Inter-ExtraBold',Helvetica] font-extrabold text-[#018bd8] text-[24px] tracking-[0] leading-[normal]">
+              Home
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
---
+++ projeto-web/src/screens/Iphone/Iphone.jsx
@@ -0,0 +1,129 @@
+import * as React from "react";
+
+export function Iphone(props) {
+  return (
+    <div className="bg-black pl-20 pr-12 py-12 max-md:px-5">
+      <div className="gap-5 flex max-md:flex-col max-md:items-stretch max-md:gap-0">
+        <div className="flex flex-col items-stretch w-[67%] max-md:w-full max-md:ml-0">
+          <div className="flex flex-col items-stretch my-auto max-md:max-w-full max-md:mt-10">
+            <div className="flex flex-col w-[1132px] max-w-full px-20 max-md:px-2">
+              <div className="text-sky-600 text-4xl font-extrabold self-stretch whitespace-nowrap ml-2 mr-2 -mb-10 max-md:max-w-full max-md:mr-2.5">
+                Cadastrar
+              </div>
+              <div className="text-sky-600 text-4xl font-extrabold self-stretch whitespace-nowrap ml-[140px] mr-4 pl-10 pr-16 max-md:max-w-full max-md:mr-2.5">
+                Listagem
+                <br />
+              </div>
+              <div className="text-white text-5xl self-center max-w-[898px] mt-14 max-md:max-w-full max-md:text-4xl max-md:mt-10">
+                <font face="Krona One, sans-serif">
+                  Transforme sua empresa em um modelo de eficiência com nossas
+                  soluções.{" "}
+                </font>
+              </div>
+              <div className="text-cyan-400 text-5xl self-stretch w-full mr-4 mt-2 max-md:text-4xl max-md:mr-2.5">
+                <font face="Krona One, sans-serif">
+                  A automação <br />é o caminho para o sucesso!
+                </font>
+              </div>
+              <div className="text-white text-2xl self-stretch max-w-[933px] ml-1 mr-4 mt-1.5 max-md:max-w-full max-md:mr-2.5 max-sm:-ml-1">
+                <font face="Krona One, sans-serif">
+                  Transforme sua empresa em um modelo de eficiência com nossas
+                  soluções de cadastro e monitoramento de funcionários. A
+                  automação é o caminho para o sucesso! Na busca pela excelência
+                  operacional, nossa plataforma simplifica processos, economiza
+                  tempo e impulsiona o crescimento do seu negócio. Com as
+                  ferramentas certas, sua empresa estará preparada para
+                  enfrentar os desafios do mercado e prosperar no mundo moderno
+                  dos negócios.
+                </font>
+              </div>
+            </div>
+            <div className="mt-6 max-md:max-w-full">
+              <div className="gap-5 flex max-md:flex-col max-md:items-stretch max-md:gap-0">
+                <div className="flex flex-col items-stretch w-[33%] max-md:w-full max-md:ml-0">
+                  <div className="flex-col fill-cyan-950 overflow-hidden relative flex aspect-[1.2157534246575343] grow items-stretch pt-5 pb-10 px-1.5 max-md:mt-8">
+                    <img
+                      loading="lazy"
+                      src="https://cdn.builder.io/api/v1/image/assets/TEMP/cbec0a9a95ce8d651fb2c03cf0f901ad0c355c99bea49ea54a925947889f38fd?"
+                      className="absolute h-full w-full object-cover object-center inset-0"
+                    />
+                    <div className="relative flex items-stretch justify-between gap-2">
+                      <img
+                        loading="lazy"
+                        srcSet="https://static.thenounproject.com/png/2774773-200.png"
+                        className="aspect-[1.18] object-contain object-center w-[86px] overflow-hidden shrink-0 max-w-full"
+                      />
+                      <div className="text-black text-center text-2xl font-extrabold grow shrink basis-auto mt-6 self-end">
+                        <font face="Inter, sans-serif">
+                          Aqui você cadastrará seus dados
+                        </font>
+                      </div>
+                    </div>
+                    <div className="relative text-white text-center text-2xl mt-7">
+                      Com toda a nossa estrutura, queremos que você se sinta o
+                      mais confortável em cadastrar seus
+                      <br />
+                      funcionários.
+                    </div>
+                  </div>
+                </div>
+                <div className="flex flex-col items-stretch w-[33%] ml-5 max-md:w-full max-md:ml-0">
+                  <div className="bg-cyan-950 flex w-full grow flex-col items-stretch mx-auto pl-3.5 pr-8 pt-3.5 pb-12 rounded-xl max-md:mt-9 max-md:pr-5">
+                    <div className="flex items-stretch justify-between gap-4">
+                      <img
+                        loading="lazy"
+                        src="https://cdn.builder.io/api/v1/image/assets/TEMP/cf55fd691556aaaf3aed56c0f31036115815f2f1ca88742d70d0ca6f509f3208?"
+                        className="aspect-[1.03] object-contain object-center w-[90px] overflow-hidden shrink-0 max-w-full"
+                      />
+                      <div className="text-black text-center text-2xl font-extrabold self-center grow whitespace-nowrap my-auto">
+                        <font face="Inter, sans-serif">Simplicidade</font>
+                      </div>
+                    </div>
+                    <div className="text-white text-center text-2xl ml-3.5 mt-8 self-start max-md:ml-2.5">
+                      Aqui terá todos os dados pessoais de todos os seus
+                      funcionários, onde você conseguirá monitorá-los.
+                    </div>
+                  </div>
+                </div>
+                <div className="flex flex-col items-stretch w-[33%] ml-5 max-md:w-full max-md:ml-0">
+                  <div className="bg-cyan-950 flex w-full grow flex-col items-center mx-auto pb-8 rounded-xl max-md:mt-9">
+                    <div className="flex items-stretch gap-px">
+                      <img
+                        loading="lazy"
+                        srcSet="https://icons.veryicon.com/png/o/object/material-design-icons/fingerprint-5.png"
+                        className="aspect-[0.7] object-contain object-center w-[70px] overflow-hidden shrink-0 max-w-full"
+                      />
+                      <div className="text-black text-center text-2xl font-extrabold self-center grow shrink basis-auto my-auto">
+                        <font face="Inter, sans-serif">Segurança avançada</font>
+                      </div>
+                    </div>
+                    <div className="text-white text-center text-2xl self-stretch mt-7">
+                      Tratamos com segurança tanto para a empresa determinada
+                      quanto para o funcionário, de acordo com a<br />
+                      Política de Privacidade.
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="flex flex-col items-stretch w-[33%] ml-5 max-md:w-full max-md:ml-0">
+          <div className="flex flex-col items-stretch my-auto max-md:max-w-full max-md:mt-10">
+            <img
+              loading="lazy"
+              srcSet="https://i.pinimg.com/564x/bf/c3/7a/bfc37a503d8fcf4d8dbf1278efbe4608.jpg"
+              className="aspect-[0.67] object-contain object-center w-full overflow-hidden max-md:max-w-full"
+            />
+            <div className="flex-col text-sky-600 text-4xl font-extrabold relative overflow-hidden self-center aspect-[3] justify-center items-stretch mt-3.5 pl-8 pr-6 py-6 max-md:px-5">
+             
+              Cadastrar
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+
